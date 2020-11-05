e5fcb006c5de28a11c8cced6080bd3c06d155a6c
Theo Sun <theo.sun@sap.com>
2020-11-05 17:18:40 +0800

    chore: update ut related

--- test/describe.ts
+++ test/describe.ts
@@ -1,56 +1,45 @@
 // @ts-nocheck
 import { expect } from "https://cdn.jsdelivr.net/gh/Soontao/expect@0.3.1/mod.ts";
+import { gray, green } from "https://deno.land/std@0.76.0/fmt/colors.ts";
 
-window.expect = expect
+window.expect = expect;
 
-const describe = (suite: string, fn: (...args: any[]) => void) => {
+const greenName = (...parts: string[]) => {
+  return parts.map(green).join(" > ");
+};
 
-  const it = (name: string, fn: (...args: any[]) => void) => {
+const grayName = (...parts: string[]) => {
+  return parts.map(gray).join(" > ");
+};
+
+const describe = (
+  suite: string,
+  fn: (...args: any[]) => void,
+  ignoreSuite = false,
+) => {
+  const it = (
+    name: string,
+    fn: (...args: any[]) => void,
+    ignore = ignoreSuite,
+  ) => {
     Deno.test({
-      name: `case '${suite}' - '${name}'`,
+      name: ignore ? grayName(suite, name) : greenName(suite, name),
       fn,
+      ignore,
       sanitizeResources: false,
       sanitizeOps: false,
-    })
-  }
+    });
+  };
 
   it.skip = (name: string, fn: (...args: any[]) => void) => {
-    Deno.test({
-      name: `case '${suite}' - '${name}'`,
-      fn,
-      ignore: true,
-      sanitizeResources: false,
-      sanitizeOps: false,
-    })
-  }
+    return it(name, fn, true);
+  };
 
-  fn(it)
-}
+  fn(it);
+};
 
 describe.skip = (suite: string, fn: (...args: any[]) => void) => {
-
-  const it = (name: string, fn: (...args: any[]) => void) => {
-    Deno.test({
-      name: `case '${suite}' - '${name}'`, fn,
-      fn,
-      ignore: true,
-      sanitizeResources: false,
-      sanitizeOps: false,
-    })
-  }
-
-  it.skip = (name: string, fn: (...args: any[]) => void) => {
-    Deno.test({
-      name: `case '${suite}' - '${name}'`, fn,
-      fn,
-      ignore: true,
-      sanitizeResources: false,
-      sanitizeOps: false,
-    })
-  }
-
-  fn(it)
-}
+  return describe(suite, fn, true);
+};
 
 export { describe };
-
