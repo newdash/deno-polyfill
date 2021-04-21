a87c74948113471109fa47113038fe09726cb2ae
Theo Sun <theo.sun@sap.com>
2021-04-21 13:22:34 +0800

    chore: update

---
+++ test/cycle.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+import { isCyclic } from "./cycle.ts";
+
+Deno.test("test isCycle", () => {
+
+  const obj1: any[] = []
+  const obj2: any = { obj3: undefined }
+  const obj3 = { obj2: obj2 }
+  assertEquals(isCyclic(obj1), false)
+  assertEquals(isCyclic(obj2), false)
+  obj2.obj3 = obj3
+  assertEquals(isCyclic(obj3), true)
+
+})
--- test/cycle.ts
+++ test/cycle.ts
@@ -1,24 +1,5 @@
+import { isCircular } from "https://cdn.jsdelivr.net/npm/@newdash/newdash-deno@5.18.0/isCircular.ts"
 
 export function isCyclic(obj: any): boolean {
-
-  var objects = new Set()
-
-  const detect = (obj: any): boolean => {
-    if (obj && typeof obj === 'object') {
-      if (objects.has(obj)) {
-        return true;
-      }
-      objects.add(obj);
-      for (var key in obj) {
-        if (typeof obj.hasOwnProperty == "function" && obj.hasOwnProperty(key) && detect(obj[key])) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  objects.clear()
-
-  return detect(obj);
+  return isCircular(obj)
 }
