6f5d2c7cb2a14354a236c1c375bd23d788d19a2d
Theo Sun <theo.sun@outlook.com>
2021-11-28 15:57:44 +0800

    chore(dep): update

--- test/describe.ts
+++ test/describe.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import { expect } from "https://cdn.jsdelivr.net/gh/Soontao/expect@0.4.4/mod.ts";
+import { expect } from "https://cdn.jsdelivr.net/gh/Soontao/expect@0.116.0/mod.ts";
 import { cyan, gray } from "https://deno.land/std@0.116.0/fmt/colors.ts";
 
 (window as any).expect = expect;
